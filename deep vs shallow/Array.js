bda134430d6fbdf3b9b0fedfea9d4548ea1a4c64
krishan190 <krishnanamdeo2001@gmail.com>
2025-08-24 23:43:34 +0530

    shallow vs deep copy

---
+++ deep vs shallow/Array.js
@@ -0,0 +1,15 @@
+// 🔹 Ways to Copy Arrays in JavaScript
+
+// ✅ Shallow Copy Methods
+
+// 1=> Spread operator
+// 2=> Slice()
+// 3=> Concat()
+// 4=> Array.from()
+// 5=> map()
+
+// ✅ Deep Copy Methods [it is same as for the object ]
+
+// 1=> structuredClone()
+// 2=> JSON.parse(JSON.stringigy(obj))
+// 3=> Lodash cloneDeep()
---
+++ deep vs shallow/Object.js
@@ -0,0 +1,43 @@
+// shallow copy in nested objects case will modify the parent object property value, if cloned object property value is changed.
+// but deep copy will not modify the parent object property value.
+
+// "Shallow copy tab use hota hai jab mujhe sirf ek level ke data ko duplicate karna ho – jaise user ki profile ko edit karte waqt, basic details nikal lena.
+// Lekin agar data nested hai jaise shopping cart ya address info, toh mujhe deep copy karni padti hai taki original data corrupt na ho."
+
+//original object
+
+// 🔹 Ways to Copy Objects in JavaScript
+
+// 1=> spread operator
+// 2=> Object.assign()
+
+const person = {
+  name: "krishan",
+  age: 20,
+  address: {
+    city: "Delhi",
+    country: "India",
+  },
+};
+
+// shallow copy using object.assign()
+
+const shallowCopy = Object.assign({}, person);
+
+shallowCopy.address.city = "Mumbai";
+
+console.log(person.address.city);
+console.log(shallowCopy.address.city);
+
+//Deep copy using JSON.parse() and JSON.stringify()
+
+// 1=> structuredClone()
+// 2=> JSON.parse(JSON.stringigy(obj))
+// 3=> Lodash cloneDeep()
+
+const deepCopy = JSON.parse(JSON.stringify(person));
+
+deepCopy.address.city = "Rewa";
+
+console.log(person.address.city);
+console.log(deepCopy.address.city);
