e4310e941406348b23ac4e38050f9f60a9a19866
krishan190 <krishnanamdeo2001@gmail.com>
2025-09-02 11:48:28 -0700

    closure concept commit

---
+++ currying + closure/closure.js
@@ -0,0 +1,33 @@
+function outer() {
+    let count = 0;
+
+    function inner() {
+        count++;
+        return count;
+    }
+
+    return inner;
+}
+
+const fn = outer();
+
+console.log(fn());
+console.log(fn());
+console.log(fn());
+
+
+// concept
+
+// What’s happening?
+
+// When you call outer(), it creates a local variable counter = 0.
+
+// outer returns the function inner.
+
+// Normally, after outer finishes, its variables (counter) should be gone from memory.
+
+// But because inner uses counter, JavaScript keeps counter alive in a closure.
+
+// Each time you call fn(), it still has access to the same counter — it doesn’t reset.
+
+
