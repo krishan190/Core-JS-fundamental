89e74eca4b64c8adaea80d21f5c57073fff4df5b
krishan190 <krishnanamdeo2001@gmail.com>
2026-02-03 23:39:21 +0530

    Experince js interview

---
+++ JS Interview Question/1.js
@@ -0,0 +1,38 @@
+// phases of Event propogation
+// Event targetting
+// lexical scope
+// ES6 feature
+// slice vs splice
+// map vs for Each
+// why we use reduce function
+// array method
+//
+console.log(
+  [1, 2, 3, 4, 5].map((e) => {
+    if (e > 0) return;
+    return e * 10;
+  }),
+);
+
+//localStorage vs session Storage
+// function currying
+// weekset and weekmap
+// Event bubbling and event capturing
+
+// React
+
+//Reconcialiation
+//package.json and package.lock.json
+//charat and tild operator
+// component life cycle
+// why we use higher order component
+// ways to optimize React App
+// webpack vs babel
+// redux vs context
+// components of Redux (action ,store,reducer)
+
+// HTML
+
+// formating Tags
+// what are empty elements
+// use of require attribute in HTML
