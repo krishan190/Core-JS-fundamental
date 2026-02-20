d327b10e63d93c146b12b4fed488d5d001e4b756
krishan190 <krishnanamdeo2001@gmail.com>
2026-02-20 19:00:18 +0530

    Add function to count fruit occurrences using reduce method

---
+++ JS Interview Question/8.js
@@ -0,0 +1,6 @@
+const fruits = ['apple', 'banana', 'apple', 'orange', 'banana', 'apple'];
+
+const result = fruits.reduce((acc, curr) => {
+    acc[curr] = (acc[curr] || 0) + 1;
+    return acc;
+}, {})
