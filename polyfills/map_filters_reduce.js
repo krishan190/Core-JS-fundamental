fb01ef89fe8d723070300a6b3665e252cb5a0950
krishan190 <krishnanamdeo2001@gmail.com>
2026-02-22 13:16:56 +0530

    map_fiter_reduce pollyfills

--- polyfills/map_filters_reduce.js
+++ polyfills/map_filters_reduce.js
@@ -1,60 +1,57 @@
+// pollyfills are the basically own implementation of method
+
 // map prototype
 
 Array.prototype.myMap = function (callback) {
-    let newArray = [];
-    for (let i = 0; i < this.length; i++) {
-        // // callback(currentValue, index, array)
-        newArray.push(callback(this[i], i, this));
-    }
-    return newArray;
-}
-
+  let newArray = [];
+  for (let i = 0; i < this.length; i++) {
+    // // callback(currentValue, index, array)
+    newArray.push(callback(this[i], i, this));
+  }
+  return newArray;
+};
 
 const arr = [1, 3, 4, 5];
 
-arr1 = arr.myMap(e => e * 2);
+arr1 = arr.myMap((e) => e * 2);
 console.log(arr1);
-
 
 // Filter prototype
 
 Array.prototype.myFilter = function (callback) {
-    let newArray = [];
+  let newArray = [];
 
-    for (let i = 0; i < this.length; i++) {
-        if (callback(this[i], i, this)) {   // ✅ only push if condition true
-            newArray.push(this[i], i, this);
-        }
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      // ✅ only push if condition true
+      newArray.push(this[i], i, this);
     }
+  }
 
-    return newArray;
+  return newArray;
 };
 
 // Example
 const arr1 = [1, 3, 4, 5];
-const filtered = arr1.myFilter(e => e % 2 === 0);
+const filtered = arr1.myFilter((e) => e % 2 === 0);
 console.log(filtered); // [4]
 
-
 // reduce prototype
-
+// arr.reduce((acc,curr,i,arr)=>{},intialValue)
 Array.prototype.myReduce = function (callback, initialValue) {
-    let accumulator = initialValue;
-    let startIndex = 0;
+  let accumulator = initialValue;
 
-    if (startIndex === undefined) {
-        accumulator = this[0];
-        startIndex = 1;
-    }
+  for (let i = 0; i < this.length; i++) {
+    accumulator = accumulator
+      ? callback(accumulator, this[i], i, this)
+      : this[i];
+  }
+  return accumulator;
+};
 
-    for (let i = startIndex; i < this.length; i++) {
-        accumulator = callback(accumulator, this[i], i, this);
-    }
-    return accumulator;
+const nums = [1, 2, 3, 4];
+const sum = nums.myReduce((acc, curr, i, arr) => {
+  return acc + curr;
+}, 0);
 
-}
-
-const arr2 = [1, 2, 3];
-
-const product = arr2.myReduce((acc, num) => acc * num, 1)
-console.log("product value", product);
+console.log("sum", sum);
